eb7b990a722e96d85b31a35d8a3e6cb8c1223e14
GabrielMtins <gabrielmartinsthe12381@gmail.com>
2025-02-28 16:46:12 -0300

    copyright notice

--- src/r_json.c
+++ src/r_json.c
@@ -2,7 +2,7 @@
 
 /*
  *	  This file is part of r_json.
- *	  Foobar is free software: you can redistribute it and/or modify it
+ *	  r_json is free software: you can redistribute it and/or modify it
  *	  under the terms of the GNU General Public License as published by the
  *	  Free Software Foundation, either version 3 of the License, or (at
  *	  your option) any later version.
@@ -14,6 +14,7 @@
  *
  *	  You should have received a copy of the GNU General Public License
  *	  along with r_json. If not, see <https://www.gnu.org/licenses/>.
+ *	  Copyright	Gabriel Martins (C) 2025
 */
 
 /* A biblioteca não depende da standard library, por isso a definição de NULL */
